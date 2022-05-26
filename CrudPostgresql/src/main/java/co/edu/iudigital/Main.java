1821f79fc358f99ed26a03e5d7671377e405e15a
zabaladev <juanzabaladev@gmail.com>
2022-05-26 10:53:36 -0500

    listar e insertar datos

--- CrudPostgresql/src/main/java/co/edu/iudigital/Main.java
+++ CrudPostgresql/src/main/java/co/edu/iudigital/Main.java
@@ -1,14 +1,21 @@
 package co.edu.iudigital;
-import co.edu.iudigital.migrations.ConfigMigration;
-import co.edu.iudigital.migrations.UserMigration;
+import co.edu.iudigital.dtos.UserDto;
+import co.edu.iudigital.models.UserModel;
+
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        ConfigMigration configMigration = new ConfigMigration();
-        configMigration.init();
-        UserMigration userMigration = new UserMigration();
-        userMigration.userSeed();
+        //ConfigMigration configMigration = new ConfigMigration();
+        //configMigration.init();
+        //UserMigration userMigration = new UserMigration();
+        //userMigration.userSeed();
+        UserModel userModel = new UserModel();
+        List<UserDto> users = userModel.getUsers();
+        for(UserDto user: users){
+            System.out.println(user.getName());
+        }
     }
 }
---
+++ CrudPostgresql/src/main/java/co/edu/iudigital/models/UserModel.java
@@ -0,0 +1,79 @@
+package co.edu.iudigital.models;
+
+import co.edu.iudigital.db.DbConfig;
+import co.edu.iudigital.dtos.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class UserModel {
+    private DbConfig dbConfig;
+    private Connection connection;
+
+    public UserModel(){
+        dbConfig = new DbConfig("localhost", "5432", "enfasis2_martes");
+        connection = dbConfig.connect();
+    }
+    public void insertPerson(int id, String name, String lastname){
+
+        String query = String.format("INSERT INTO persons (id, name, lastname) VALUES(%d, '%s','%s')", id, name, lastname);
+        try{
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+            statement.close();
+            connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<UserDto> getUsers(){
+        String query = "SELECT * FROM users";
+        List<UserDto> users = new ArrayList<>();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()){
+                UserDto userDto = new UserDto();
+                userDto.setId(Integer.parseInt(result.getString("id")));
+                userDto.setName(result.getString("name"));
+                userDto.setLastname(result.getString("lastname"));
+                users.add(userDto);
+            }
+            statement.close();
+            connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return users;
+    }
+    public List<UserDto> getUsers(int id){
+        String query = "SELECT * FROM users WHERE id=" + id;
+        List<UserDto> users = new ArrayList<>();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()){
+                UserDto userDto = new UserDto();
+                userDto.setId(Integer.parseInt(result.getString("id")));
+                userDto.setName(result.getString("name"));
+                userDto.setLastname(result.getString("lastname"));
+                users.add(userDto);
+            }
+            statement.close();
+            connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return users;
+    }
+}
